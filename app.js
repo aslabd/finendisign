0dec3c819693eb7bc742d86f0d48cfc6edd2916d
aslabd <aslamabdurrohim@gmail.com>
2018-12-16 22:37:23 +0700

    add func to create user

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require(path.join(__dirname, 'routes', 'index'));
+var usersRouter = require(path.join(__dirname, 'routes', 'users'));
 
 var app = express();
 
--- controllers/users.js
+++ controllers/users.js
@@ -3,16 +3,17 @@ var express = require('express'),
 	path = require('path')
 
 var sequelize = require('../configuration/database');
+var Op = sequelize.Op
 
 var User = sequelize.import(__dirname + "/../models/users");
 
-var validateEmail = function(email) {
+var isEmail = function(email) {
 	var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 	return email.match(regex);
 }
 
 function UsersControllers() {
-	this.login = function(req, res){
+	this.login = function(req, res) {
 		let username = req.body.username,
 			password = req.body.password,
 			remember_me = req.body.remember_me;
@@ -60,6 +61,53 @@ function UsersControllers() {
 		    	.catch(function(err) {
 		    		res.json({status: {success: false, code: 500}, message: 'Login gagal!', err: err});
 		    	});
+		}
+	}
+
+	this.create = function(req, res) {
+		let username = req.body.username,
+			name = req.body.name,
+			email = req.body.email,
+			role = req.body.role,
+			password = req.body.password,
+			confirm_password = req.body.confirm_password
+
+		if (!username || !name || !email || !role || !password || !confirm_password) {
+			res.json({status: {success: false, code: 400}, message: 'Ada form yang kosong.'})
+		} if (!isEmail(email)) {
+			res.json({status: {success: false, code: 400}, message: 'Format email salah.'})
+		} else if (confirm_password != password) {
+			res.json({status: {success: false, code: 400}, message: 'Password beda dengan konfirmasi.'})
+		} else {
+			User
+				.findAll({
+					where: {
+						[Op.or]: [{username: username}, {email: email}]
+					}
+				})
+				.then(function(user) {
+					if (user != 0) {
+						res.json({status: {success: false, code: 400}, message: 'Username atau email sudah ada.'})
+					} else {
+						User
+							.create({
+								username: username,
+								name: name,
+								email: email,
+								role: role,
+								password: crypto.createHash('sha256').update(password).digest('hex')
+							})
+							.then(function(user) {
+								res.json({status: {success: true, code: 200}, message: 'Buat user berhasil.', data: user})
+							})
+							.catch(function(err) {
+								res.json({status: {success: false, code: 500}, message: 'Buat user gagal.', err: err})
+							})
+					}
+				})
+				.catch(function(err) {
+					res.json({status: {success: false, code: 500}, message: 'Cari user gagal.', err: err});
+				})
 		}
 	}
 }
