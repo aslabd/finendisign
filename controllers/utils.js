4340e2e8c41f53cf485ca0b83117dd448efd07a6
aslamabdurrohim <aslam.abdurrohim@tokopedia.com>
2019-01-18 08:59:08 +0700

    bugfix

--- controllers/utils.js
+++ controllers/utils.js
@@ -3,18 +3,24 @@ var path = require('path')
 var commonConfiguration = require(path.join(__dirname, '/../configuration/common'));
 
 function UtilsControllers() {
+	// Validator
 	this.isEmail = function(email) {
 		var regex = commonConfiguration.regex.email;
 		return email.match(regex);
 	}
 
 	this.isPhoneNumber = function(phoneNumber) {
-
+		var regex = commonConfiguration.regex.phoneNumber;
+		return phoneNumber.match(regex);
 	}
 
 	this.isImageURL = function(imageURL) {
 		var regex = commonConfiguration.regex.imageURL;
 		return imageURL.match(regex);
+	}
+
+	this.deleteValueFromArrayString = function(array) {
+		
 	}
 }
 
