09661863c04a33f77ff80e001e468431ac459f33
Muhammad Aslam Abdurrohim <aslam.abdurrohim@tokopedia.com>
2019-03-28 19:31:58 +0700

    body parser

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
+var bodyParser = require('body-parser')
 
 var indexRouter = require(path.join(__dirname, 'routes', 'index'));
 var usersRouter = require(path.join(__dirname, 'routes', 'users'));
@@ -21,8 +22,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use(cors());
